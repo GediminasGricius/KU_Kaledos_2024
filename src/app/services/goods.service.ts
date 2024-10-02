6f00c098a9eafa64006f42c76bbd3c4936a01674
GediminasGricius <88775405+GediminasGricius@users.noreply.github.com>
2024-10-02 18:14:37 +0300

    Update

--- src/app/services/goods.service.ts
+++ src/app/services/goods.service.ts
@@ -10,11 +10,22 @@ export class GoodsService {
   constructor(private http:HttpClient) { }
 
   public addGood(item:Good){
-    this.http.post("https://kaledos2024-e4d4f-default-rtdb.europe-west1.firebasedatabase.app/goods.json", item).subscribe(()=>{})
+    return this.http.post("https://kaledos2024-e4d4f-default-rtdb.europe-west1.firebasedatabase.app/goods.json", item);
   }
 
   public loadGoods(){
     return this.http.get<{[key:string]:Good}>("https://kaledos2024-e4d4f-default-rtdb.europe-west1.firebasedatabase.app/goods.json");
+  }
+
+  public loadGood(id:string){
+    return this.http.get<Good>("https://kaledos2024-e4d4f-default-rtdb.europe-west1.firebasedatabase.app/goods/"+id+".json");
+  }
+  public updateRecord(item:Good){
+    return this.http.patch("https://kaledos2024-e4d4f-default-rtdb.europe-west1.firebasedatabase.app/goods/"+item.id+".json", item);
+  }
+
+  public deleteGood(id:string){
+    return this.http.delete("https://kaledos2024-e4d4f-default-rtdb.europe-west1.firebasedatabase.app/goods/"+id+".json");
   }
 
 }
